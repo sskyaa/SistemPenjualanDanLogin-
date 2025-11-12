6238c509601aedcc822e2118e73733adfb7c8af9
sskyaa <saskiatripani@gmail.com>
2025-11-12 21:01:04 +0700

    Mendesain Halaman Login

---
+++ LOGIN/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <h2>FORM LOGIN</h2>
+        <form method="post">
+            <label for="username">👤Username:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="password">🔒Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit">Login</button>
+            <button type="reset">Batal</button>
+        </form>
+    </div>
+
+</body>
+</html>
