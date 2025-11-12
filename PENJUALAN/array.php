ffec6e415a72be473756e28dceb32455ebf677ea
sskyaa <saskiatripani@gmail.com>
2025-11-12 22:33:23 +0700

    Dashboard Penjualan

---
+++ PENJUALAN/array.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2> === POLGAN MART === </h2>";
+
+$nama_barang   = ["Tas Ransel", "Sepatu", "Seragam", "Hijab Segi Empat", "Dasi"];
+$harga_barang  = [120000, 200000, 400000, 30000, 10000];
+$kode_barang   = ["AR01", "AR02", "AR03", "AR04", "AR05"];
